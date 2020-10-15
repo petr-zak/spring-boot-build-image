6620101934de1480942539ad367f047add24f4cf
petr-zak <petrzak79@gmail.com>
2020-10-15 14:43:08 +0200

    init commit

---
+++ src/test/java/cz/test/dockertest/DockertestApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.test.dockertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockertestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
